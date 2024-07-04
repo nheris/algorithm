0d49a3bcd4a0efac874811c281ec2de20cd6b2a0
nheris <147366904+nheris@users.noreply.github.com>
2024-07-04 16:41:10 +0900

    크로아티아 알파벳.java

--- 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.java
+++ 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.java
@@ -9,51 +9,49 @@ public class Main {
 		String str = br.readLine();
 		
 		int len = str.length();
-		int count = 0;
+		int num = 0;
  
 		for (int i = 0; i < len; i++) {
  
-			char ch = str.charAt(i);
+			char cha = str.charAt(i);
  
-			if(ch == 'c' && i < len - 1) {			// 만약 ch 가 c 라면?
-				//만약 ch 다음 문자가 '=' 또는 '-' 이라면?
+			if(cha == 'c' && i < len - 1) {	
 				if(str.charAt(i + 1) == '=' || str.charAt(i + 1) == '-') {		
-					// i+1 까지가 하나의 문자이므로 다음 문자를 건너 뛰기 위해 1 증가
 					i++;		
 				}
 				
 			}
 		    
-			else if(ch == 'd' && i < len - 1) {
-				if(str.charAt(i + 1) == '-') {	// d- 일 경우
+			else if(cha == 'd' && i < len - 1) {
+				if(str.charAt(i + 1) == '-') {	
 						i++;
 					}
 				else if(str.charAt(i + 1) == 'z' && i < len - 2) {
 					
-					if(str.charAt(i + 2) == '=') {	// dz= 일 경우
+					if(str.charAt(i + 2) == '=') {	
 						i += 2;
 					}
 				}
 			}
 		    
-			else if((ch == 'l' || ch == 'n') && i < len - 1) {
-				if(str.charAt(i + 1) == 'j') {	// lj 또는 nj 일 경우
+			else if((cha == 'l' || cha == 'n') && i < len - 1) {
+				if(str.charAt(i + 1) == 'j') {
 					i++;
 				}
 			}
 		    
  
-			else if((ch == 's' || ch == 'z') && i < len - 1) {
-				if(str.charAt(i + 1) == '=') {	// s= 또는z= 일 경우
+			else if((cha == 's' || cha == 'z') && i < len - 1) {
+				if(str.charAt(i + 1) == '=') {
 					i++;
 				}
 			
 		    }
 		    
-			count++;
+			num++;
  
 		}
  
-		System.out.println(count);
+		System.out.println(num);
 	}
 }
