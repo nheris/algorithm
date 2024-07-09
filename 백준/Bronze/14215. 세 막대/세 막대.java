708f53f72b5fe79e1780f5fa731bba88e4153212
nheris <147366904+nheris@users.noreply.github.com>
2024-07-09 21:53:15 +0900

    [Bronze III] Title: 세 막대, Time: 124 ms, Memory: 14264 KB -BaekjoonHub

---
+++ 백준/Bronze/14215. 세 막대/세 막대.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+    
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine()," ");
+	
+		br.close();
+		int[] arr = new int[3];
+		
+		arr[0] = Integer.parseInt(st.nextToken());
+		arr[1] = Integer.parseInt(st.nextToken());
+		arr[2] = Integer.parseInt(st.nextToken());
+		
+		Arrays.sort(arr);
+		
+		if(arr[2]>=arr[0]+arr[1]) {
+			arr[2]=arr[0]+arr[1]-1;			
+		}
+		
+		System.out.println(arr[2]+arr[0]+arr[1]);
+	}
+}
