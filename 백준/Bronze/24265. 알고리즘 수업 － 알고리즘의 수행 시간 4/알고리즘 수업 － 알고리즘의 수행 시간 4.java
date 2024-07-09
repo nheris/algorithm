1fb307829f7f68eb0f093f4b2ff691389a6e5be1
nheris <147366904+nheris@users.noreply.github.com>
2024-07-10 01:11:40 +0900

    [Bronze III] Title: 알고리즘 수업 - 알고리즘의 수행 시간 4, Time: 120 ms, Memory: 14148 KB -BaekjoonHub

---
+++ 백준/Bronze/24265. 알고리즘 수업 － 알고리즘의 수행 시간 4/알고리즘 수업 － 알고리즘의 수행 시간 4.java
@@ -0,0 +1,15 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+    
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	
+		Long num = Long.parseLong(br.readLine());
+		
+		System.out.println(num*(num-1)/2);
+		System.out.println('2');
+	}
+}
